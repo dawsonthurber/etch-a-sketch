db7f0c6396379114e720c590130b36d410ba502b
dawsonthurber <dawson.thurber@gmail.com>
2025-09-18 09:44:53 -0500

    Add sketch area
    A grid of 16x16 squares have been added to the
    screen, and when hovered over, will change color.

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const gridContainer = document.querySelector("#gridContainer");
+const width = gridContainer.clientWidth;
+
+gridContainer.addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("grid")) {
+    e.target.style.backgroundColor = "white";
+
+    let currentOpacity = parseFloat(e.target.style.opacity);
+    if (!Number.isNaN(currentOpacity)) {
+      if (currentOpacity < 1) {
+        e.target.style.opacity = `${(currentOpacity += 0.2)}`;
+      }
+    } else {
+      e.target.style.opacity = "0.1";
+    }
+  }
+});
+
+for (let i = 0; i < 16 * 16; i++) {
+  const grid = document.createElement("div");
+  grid.classList = "grid";
+  grid.style.backgroundColor = "#000000";
+  grid.style.width = `${Math.floor(width / 16)}px`;
+  gridContainer.appendChild(grid);
+}
